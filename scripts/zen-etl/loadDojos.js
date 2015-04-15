f4d1c39ca42f5fa1668e01b0de44f826472a99ca
sigismondm <sigismond.mclaughlin@nearform.com>
2015-04-15 11:09:11 -0500

    Add loadGeonames script and modify etl

--- scripts/zen-etl/loadDojos.js
+++ scripts/zen-etl/loadDojos.js
@@ -33,17 +33,19 @@ seneca.ready(function() {
       function createDojo(dojo, cb){
 
         var query = "INSERT INTO cd_dojos(id, mysql_dojo_id, name, creator, created, verified_at, verified_by," +
-                    "verified, need_mentors, stage, time, country, location, coordinates, notes, email, " + 
+                    "verified, need_mentors, stage, time, location, coordinates, notes, email, " + 
                     "website, twitter, google_group, eb_id, supporter_image, deleted, deleted_by, deleted_at," +
                     "private, url_slug, continent, alpha2, alpha3, country_number, country_name)" + 
                     " VALUES ('" + dojo.id + "','" + dojo.mysql_dojo_id + "','" + pgEscapeString(dojo.name) + "','"+
                      dojo.creator + "','" + dojo.created + "','"  + dojo.verified_at + "','"  +
                      dojo.verified_by + "'," + dojo.verified + "," + dojo.need_mentors + "," +
-                     dojo.stage + ",'" + pgEscapeString(dojo.time) + "','" + dojo.country + "','" + pgEscapeString(dojo.location) + "','" +
+                     dojo.stage + ",'" + pgEscapeString(dojo.time) + "','" + pgEscapeString(dojo.location) + "','" +
                      pgEscapeString(dojo.coordinates) + "','" + pgEscapeString(dojo.notes) + "','" + dojo.email + "','" + dojo.website + "','" +
                      dojo.twitter + "','" + dojo.google_group + "','" + dojo.eb_id + "','" + dojo.supporter_image + "','" +
                      dojo.deleted + "','" + dojo.deleted_by + "','"  + dojo.deleted_at + "','" + dojo.private + "','" +  dojo.url_slug + 
                      "','" + dojo.continent +  "','"  + dojo.alpha2 + "','" + dojo.alpha3 + "'," + dojo.country_number + ",'" + pgEscapeString(dojo.country_name) + "')";
+
+        console.log("query", query);
         
         client.query(query, function(err, result) {
           if(err) {
