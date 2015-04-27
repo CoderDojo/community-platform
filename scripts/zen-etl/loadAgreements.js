8ec3e35d5792afc18c3b3a6754c8041dfe382591
sigismondm <sigismond.mclaughlin@nearform.com>
2015-04-27 08:43:35 -0500

    import agreements via seneca

--- scripts/zen-etl/loadAgreements.js
+++ scripts/zen-etl/loadAgreements.js
@@ -7,52 +7,32 @@ var async = require('async');
 var agreements = JSON.parse(fs.readFileSync('./data/agreements.json', 'utf8'));
 var plugin = "load-agreements";
 var config = require('config');
-var pg = require('pg');
 
-var conString = config.postgresql.connstring;
+var ENTITY_NS = "cd/agreements"
+var _ = require("lodash");
 
-function pgEscapeString(str){
-  return str.split("'").join("''");
-}
+seneca.use('postgresql-store', config['postgresql-store']);
+
 seneca.ready(function(){
   seneca.add({role: plugin, cmd: 'insert'}, function(args, done){
-    var client = new pg.Client(conString);
 
-    client.connect(function(err){
-      if(err){
-        console.error('could not connect to postgres', err);
-        return done(err);
-      }
+    function createAgreemnt(agreement, cb){
+      agreement.id$ = agreement.id;
+      delete agreement.id;
 
-      function createAgreemnt(agreement, cb){
-        var query = 'INSERT INTO cd_agreements(mysql_user_id, full_name, ip_address, "timestamp", agreement_version, user_id, id)' + 
-                    " VALUES(" + agreement.mysql_user_id + ",'" + pgEscapeString(agreement.full_name) + "', '" +  agreement.ip_address + "', '" +
-                    agreement.timestamp + "', '" + agreement.agreement_version + "', '"  +  agreement.user_id + "', '" +
-                    agreement.id + "')";
+      var agreement_ent = seneca.make(ENTITY_NS);
+      _.assign(agreement_ent, agreement);
 
-        client.query(query, function(err, result){
-          if(err){
-            console.error('error running query', err);
-            return cb(err);
-          }
+      agreement_ent.save$(cb);
+    }
 
-          return cb();
-        });
-      }
+    var loadAgreements = function(done){
+      async.eachSeries(agreements, createAgreemnt, done);
+    };
 
-      var loadAgreements = function(done){
-        async.eachSeries(agreements, createAgreemnt, done);
-      }
-
-      async.series([loadAgreements], function(err){
-        if(err){
-          return done(err);
-        }
-
-        client.end();
-        done();
-      })
-    });
+    async.series([
+      loadAgreements
+    ], done);
   });
 
   seneca.act({ role: plugin, cmd: 'insert', timeout: false }, function(err){
