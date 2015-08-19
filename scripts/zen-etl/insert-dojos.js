67ae62d9506cfd3fb8a30c6abc534f0afe0cfc27
Davey Moran <davey.moran@nearform.com>
2015-08-19 13:09:09 +0100

    adding salesforce functionality

--- scripts/zen-etl/insert-dojos.js
+++ scripts/zen-etl/insert-dojos.js
@@ -1,17 +1,38 @@
 'use strict';
 var _ = require('lodash');
+var fs= require('fs');
 var async = require('async');
+var envvars = require('./migration-vars.env');
 var usersData = require('./data/users.json');
-var dojosData = require('./data/dojos.json');
+//var dojosData = require('./data/dojos.json');
+var dojosData = require('./data/salesforce-dojo.json');
 
+_.each(envvars, function(value, key) {
+  process.env[key] = value;
+});
+
+var today = new Date();
+var datetag = (today.getFullYear()).toString()+(today.getMonth()+1)+(today.getDate())+(today.getHours())+((today.getMinutes()<10)?"0"+today.getMinutes():today.getMinutes());
+var sflogfile= "./salesforce-dojos-migration-logfile."+datetag+".txt";
+fs.exists(sflogfile, function (exists) {
+  if(!exists) fs.writeFile(sflogfile);
+});
+
+logOutput("--- starting dojo migration(" + new Date() + ") ===\n",sflogfile)
+var leadRecType= process.env.SALESFORCE_LEAD_RECORDTYPEID;
 
 var config = {
+  salesforce: {
+    loginUrl: process.env.SALESFORCE_URL,
+    username: process.env.SALESFORCE_USERNAME,
+    password: process.env.SALESFORCE_PASSWORD
+  },
   'postgresql-store': {
-      name:  process.env.DOJOS_DB_NAME || 'phase3-cp-dojos-development',
-      host: process.env.POSTGRES_HOST || 'localhost',
-      port: process.env.POSTGRES_PORT || 5432,
-      username: process.env.POSTGRES_USERNAME || 'platform',
-      password: process.env.POSTGRES_PASSWORD || 'QdYx3D5y'
+    name:  process.env.DOJOS_DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD
   }
 }
 
@@ -25,7 +46,8 @@ seneca.use('postgresql-store', config["postgresql-store"]);
 
 seneca
   .use('user')
-  .client({port:10301, pin:'role:cd-dojos,cmd:*', type:'tcp'});
+  .client({port:10301, pin:'role:cd-dojos,cmd:*', type:'tcp'})
+  .client({type: 'tcp', port: 10304, pin: 'role:cd-salesforce,cmd:*'});
 
 seneca.ready(function() {
   async.eachSeries(dojosData, createDojo, function (err) {
@@ -36,7 +58,7 @@ seneca.ready(function() {
       console.log('dojo-data inserted successfully');
     }
 
-    seneca.close(process.exit);
+    //seneca.close(process.exit);
   });
 
   var count = 0;
@@ -79,8 +101,93 @@ seneca.ready(function() {
       dojoLead.application = {};
       dojoLead.completed = true;
       if((dojo.verified == 1) || (dojo.verified != 1 && dojo.verified_by != null)) dojoLead.converted = true;
-      seneca.act({role: 'cd-dojos', cmd: 'save_dojo_lead', dojoLead: dojoLead}, cb);
+      seneca.act({role: 'cd-dojos', cmd: 'save_dojo_lead', dojoLead: dojoLead}, function(err, res) {        
+        if(err) return cb(err);
+        if(!res.id) return cb(res);
+
+        if(dojo.creator) {
+          var leadObj= {
+            verified: dojo.verified || 0,
+            verified_by: dojo.verified_by || null,
+            dojoLeadId: res.id,
+            creator: dojo.creator,
+            lead: {
+              PlatformId__c: res.id,
+              PlatformUrl__c: 'https://zen.coderdojo.com/dashboard/profile/' + res.id,
+              RecordTypeId: leadRecType,
+              Company: dojo.name || '<no name specified>',
+              LastName: dojo.name || '<no name specified>',
+              Email: dojo.email || '<nobody@email.com>',
+              Phone: dojo.phone || '00000000',
+              Time__c: dojo.time  || null,
+              Country: (dojo.country) ? dojo.country.countryName : null,
+              City: (dojo.place) ? dojo.place.name : null,
+              State: (dojo.state) ? dojo.state.toponymName : null,
+              Street: dojo.address1 || null,
+              Coordinates__Latitude__s: (dojo.coordinates) ? dojo.coordinates.split(',')[0] : null,
+              Coordinates__Longitude__s: (dojo.coordinates) ? dojo.coordinates.split(',')[1] : null,
+              Notes__c: dojo.notes || null,
+              NeedMentors__c: dojo.need_mentors || 0,
+              Stage__c: dojo.stage || 0,
+              Private__c: dojo.private || 0,
+              GoogleGroupURL__c: dojo.google_group || null,
+              Website: dojo.website || null,
+              Twitter__c: dojo.twitter || null,
+              Language__c: 'en_US',
+              Deleted__c: dojo.deleted || 0,
+              Status: '5. Dojo Listing Created'
+            }
+          };
+          updateSalesForce(leadObj, function(message) { logOutput(message, sflogfile); });
+          return cb();
+        } else {
+          logOutput(leadObj.dojoLeadId + ": not created - no creator", sflogfile);
+          return cb();
+        }
+      });
     }
   }
 });
+
+function updateSalesForce(leadObj, cb) {
+      
+  seneca.act('role:cd-salesforce,cmd:get_account', {accId: leadObj.creator}, function (err, res){
+    if(err) return err;
+    if(!res.accId) return cb(leadObj.dojoLeadId+": error retrieving champion account");
+
+    leadObj.lead.ChampionAccount__c= res.accId;
+    seneca.act('role:cd-salesforce,cmd:save_lead', {userId: leadObj.lead.PlatformId__c, lead: leadObj.lead}, function (err, res){
+      if(err) return err;
+      if(!res) return cb(leadObj.dojoLeadId+": error saving lead");
+
+      if((leadObj.verified == 1 ) || (leadObj.verified != 1 && leadObj.verified_by != null)) {
+        seneca.act('role:cd-salesforce,cmd:convert_lead_to_account', {leadId: res.id$}, function (err, res){
+          if(err) return err;
+          if(!res) return cb(leadObj.dojoLeadId+": error converting lead to account"); 
+
+          if(leadObj.verified != 1) {
+            var account= { Verified__c: 0 }
+
+            seneca.act('role:cd-salesforce,cmd:save_account', {userId: leadObj.dojoLeadId, account: account}, function (err, res){
+              if(err) return err;
+              if(!res) return cb(leadObj.dojoLeadId+": error saving newly converted account");
+
+              return cb(leadObj.dojoLeadId+": created salesforce lead and converted to account and updated converted account");
+            });
+          } else {
+            return cb(leadObj.dojoLeadId+": created salesforce lead and converted to account");
+          }
+        });
+      } else {
+        return cb(leadObj.dojoLeadId+": created salesforce lead");
+      }
+    });
+  });
+} 
+
+function logOutput(message, filename) {
+  fs.appendFile(filename, message+"\n", function(err) {
+    if(err) return console.log(err);
+  }); 
+}
 
--- scripts/zen-etl/insert-users.js
+++ scripts/zen-etl/insert-users.js
@@ -1,20 +1,42 @@
 'use strict';
 var _ = require('lodash');
+var fs= require('fs');
 var async = require('async');
-var usersData = require('./data/users.json');
+var envvars = require('./migration-vars.env');
+//var usersData = require('./data/users.json');
+var usersData= require('./data/salesforce-user.json');
+
+_.each(envvars, function(value, key) {
+  process.env[key] = value;
+});
+var today = new Date();
+var datetag = (today.getFullYear()).toString()+(today.getMonth()+1)+(today.getDate())+(today.getHours())+((today.getMinutes()<10)?"0"+today.getMinutes():today.getMinutes());
+var sflogfile= "./salesforce-users-migration-logfile."+datetag+".txt";
+fs.exists(sflogfile, function (exists) {
+  if(!exists) fs.writeFile(sflogfile);
+});
+
+logOutput("--- starting user migration(" + new Date() + ") ---\n", sflogfile);
+var accRecType= process.env.SALESFORCE_ACC_RECORDTYPEID;
 
 var config = {
   'postgresql-store': {
-      name: process.env.USERS_DB_NAME || 'phase3-cp-users-development',
-      host: process.env.POSTGRES_HOST || '127.0.0.1',
-      port: process.env.POSTGRES_PORT || 5432,
-      username: process.env.POSTGRES_USERNAME || 'platform',
-      password: process.env.POSTGRES_PASSWORD || 'QdYx3D5y'
+    name: process.env.USERS_DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD
+  },
+  salesforce: {
+    loginUrl: process.env.SALESFORCE_URL,
+    username: process.env.SALESFORCE_USERNAME,
+    password: process.env.SALESFORCE_PASSWORD
   }
 }
 
 var seneca = require('seneca')({timeout: 650000});
 var role = 'user-data';
+var userpin;
 
 seneca.log.info('using config', JSON.stringify(config, null, 4));
 seneca.options(config);
@@ -23,10 +45,11 @@ seneca.use('postgresql-store', config["postgresql-store"]);
 
 seneca
   .use('user')
-  .client({port:10303, pin:'role:cd-profiles,cmd:*', type:'tcp'});
+  .client({port:10303, pin:'role:cd-profiles,cmd:*', type:'tcp'})
+  .client({port:10304, pin:'role:cd-salesforce,cmd:*', type:'tcp'});
 
 seneca.ready(function (args, done) {
-  var userpin = seneca.pin({ role: 'user', cmd: '*'});
+  userpin = seneca.pin({ role: 'user', cmd: '*'});
 
   async.eachSeries(usersData, registerUser, function (err) {
     if (err) {
@@ -36,33 +59,65 @@ seneca.ready(function (args, done) {
       console.log('user-data inserted successfully');
     }
 
-    seneca.close(process.exit);
+    //seneca.close(process.exit);
   });
 
-  var registerUser = function (user, done) {
-    user = _.omit(user, [ 'password', 'new_password_key', 
-                          'new_password_requested','new_email', 
-                          'new_email_key', 'last_ip', 
-                          'last_login', 'created']);
-
-    user.id$ = user.id;
-    
-    //only register the champions!
-    if(user.init_user_type && user.init_user_type.name == "champion"){
-      userpin.register(user, function(err, res){
-        if (err) return done(err);
-
-        var profileData = {
-          userId:   user.id,
-          name:     user.name,
-          email:    user.email,
-          userType: user.init_user_type.name
-        };
-        seneca.act({role:'cd-profiles', cmd:'save', profile: profileData, timeout: false}, cb);
-      });
-    } else {
-      cb(null);
-    }
-  };
 });
+
+function registerUser(user, cb) {
+  user = _.omit(user, [ 'password', 'new_password_key', 
+                        'new_password_requested','new_email', 
+                        'new_email_key', 'last_ip', 
+                        'last_login', 'created']);
+
+  user.id$ = user.id;
+    
+  //only register the champions!
+  if(user.init_user_type && user.init_user_type.name == "champion"){
+    userpin.register(user, function(err, res){
+      if (err) return cb(err);
+
+      var profileData = {
+        userId:   user.id,
+        name:     user.name,
+        email:    user.email,
+        userType: user.init_user_type.name
+      };
+      seneca.act({role:'cd-profiles', cmd:'save', profile: profileData, timeout: false}, function(err, res) {
+        if(err) return cb(err);
+        if(!res) return cb(res);
+
+        var account = {
+          PlatformId__c: user.id,
+          PlatformUrl__c: 'https://zen.coderdojo.com/dashboard/profile/' + user.id,
+          Email__c: user.email || '<nobody@nowhere.com>',
+          Name: user.name ||'<nobody>',
+          RecordTypeId: accRecType,
+          UserType__c: (user.init_user_type) ? user.init_user_type.title : ""
+        };
+        updateSalesForce(account, function(message) { logOutput(message, sflogfile); });
+        return cb();
+      });
+    });
+  } else {
+    cb(null);
+  }
+};
+
+
+function updateSalesForce(account, cb) {
+  seneca.act('role:cd-salesforce,cmd:save_account', {userId: account.PlatformId__c, account: account}, function (err, res){
+      if(err) return err;
+      if(!res) return cb(account.PlatformId__c+": error saving account");
+
+      return cb(account.PlatformId__c+": account sucessfully saved");
+    });
+};
+
+function logOutput(message, filename) {
+  fs.appendFile(filename, message+"\n", function(err) {
+  //fs.writeFile(filename, message+"\n", { flags: 'w' }, function(err) {
+    if(err) return console.log(err);
+  }); 
+}
 
